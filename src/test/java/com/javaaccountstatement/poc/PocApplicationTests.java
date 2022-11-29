5d29f600bd394e33722aab61773fc883c49d52af
William Ravelo Mendez <william.ravelo@kueski.com>
2022-11-29 14:03:05 -0500

    tests

--- src/test/java/com/javaaccountstatement/poc/PocApplicationTests.java
+++ src/test/java/com/javaaccountstatement/poc/PocApplicationTests.java
@@ -3,8 +3,7 @@ package com.javaaccountstatement.poc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PocApplicationTests {
+class PocApplicationTests extends AbstractTest {
 
 	@Test
 	void contextLoads() {
